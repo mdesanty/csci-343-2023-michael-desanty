64afc583aed8d8ee69dab3ec15be52c5d1684621
Michael DeSanty <michael.desanty@gmail.com>
2023-01-25 20:51:03 -0500

    More class 1/25 - request validation and try/catch

--- OurFirstNodeServer/server.js
+++ OurFirstNodeServer/server.js
@@ -9,36 +9,49 @@ server.listen(process.env.PORT, process.env.HOST, startHandler);
 function requestHandler(req, res) {
   console.log("Handling request.");
 
-  const urlParts = url.parse(req.url, true);
-  const method = req.method;
-  const path = urlParts.pathname;
+  try {
+    const urlParts = url.parse(req.url, true);
+    const method = req.method;
+    const path = urlParts.pathname;
 
-  if (method !== "GET") {
-    writeResponse(res, 405, { error: `Method ${method} not allowed.` });
-    return;
+    if (method !== "GET") {
+      writeResponse(res, 405, { error: `Method ${method} not allowed.` });
+      return;
+    }
+
+    switch (path) {
+      case "/add":
+        add(req, res);
+        break;
+      case "/subtract":
+        subtract(req, res);
+        break;
+      case "/sum":
+        sum(req, res);
+        break;
+      default:
+        writeResponse(res, 400, { error: `<p>Invalid path ${path}.</p>` });
+        break;
+    }
   }
-
-  switch (path) {
-    case "/add":
-      add(req, res);
-      break;
-    case "/subtract":
-      subtract(req, res);
-      break;
-    case "/sum":
-      sum(req, res);
-      break;
-    default:
-      writeResponse(res, 400, { error: `<p>Invalid path ${path}.</p>` });
-      break;
+  catch (e) {
+    writeResponse(res, 400, { error: e.message });
   }
 }
 
 function add(req, res) {
   const query = getQuery(req);
 
+  if(query.a === undefined || query.b === undefined) {
+    throw Error("Both a and b are required.");
+  }
+
   const a = parseFloat(query.a);
   const b = parseFloat(query.b);
+
+  if(isNaN(a) || isNaN(b)) {
+    throw Error("Both a and b must be numbers.");
+  }
 
   const sum = a + b;
   writeResponse(res, 200, { sum: sum });
@@ -47,8 +60,16 @@ function add(req, res) {
 function subtract(req, res) {
   const query = getQuery(req);
 
+  if (query.a === undefined || query.b === undefined) {
+    throw Error("Both a and b are required.");
+  }
+
   const a = parseFloat(query.a);
   const b = parseFloat(query.b);
+
+  if (isNaN(a) || isNaN(b)) {
+    throw Error("Both a and b must be numbers.");
+  }
 
   const difference = a - b;
   writeResponse(res, 200, { difference: difference });
@@ -58,9 +79,18 @@ function sum(req, res) {
   const query = getQuery(req);
   const nums = (query.num instanceof Array ? query.num : [query.num]);
 
+  if (query.num === undefined) {
+    throw Error("num is required and must contain at least one value.");
+  }
+
   let sum = 0;
 
   nums.forEach((num) => {
+    const value = parseFloat(num);
+    if (isNaN(value)) {
+      throw Error("num can only conatain numbers.");
+    }
+
     sum += parseFloat(num);
   });
 
